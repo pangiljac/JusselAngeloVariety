d0cfb1d52581483db5aaf6af0486dc61102675a4
Jesse Angelo <pangiljac@gmail.com>
2023-05-09 21:49:07 -0700

    Created the Initial Vue Environment
    Built the initial environment with the Calculator app the Jussel will build upon,

---
+++ hello-world/src/components/Calculator.vue
@@ -0,0 +1,80 @@
+
+// Front end display
+// Shows all the details and available things to click and see
+<template> 
+    <div>
+        <h1>Calculator App</h1>
+        <!--Display-->
+        <!--Error Message-->
+        <!--10 Buttons for Numbers-->
+        <!--5 Buttons for Operation-->
+    </div>
+</template>
+
+
+// Back end
+// Mostly uses Javascript
+// Only Use
+    // methods
+    // data
+<script>
+export default {
+  name: 'Calculator',
+  data() {
+    return {
+        // 2 Values
+        // End Value
+        // Operation (string)
+    };
+  },
+  methods: {
+    // Add
+    add () {
+
+    },
+    // Subtract
+    subtrace() {
+
+    },
+    // Divide
+    divide() {
+
+    },
+    // Multiply
+    multiply() {
+
+    },
+    // Equals
+    execute() {
+        // HINT: Complete math operations HERE
+
+    },
+    // Number
+    inputNumber(number) {
+
+    }
+  },
+}
+</script>
+
+
+// Style Guide, this is how things should look
+// Look into CSS for more information
+
+
+// Primitive Types 
+    // CHAR 'a' 'n' '?' ' ' '1'
+    // STRING "Jussel" "123" "10.10"
+    // INTEGER 10
+    // FLOAT 10.4523
+    // BOOLEAN true/false
+
+// Data Types
+    // ARRAY = [SAME TYPE] [5, 2, 8] ARRAY[0] = 5, ARRAY[2] = 8
+    // OBJECT = {} OBJECT[NAME] = Jussel
+        /*{
+            NAME: "Jussel",
+            age: 20,
+            isCoding: true,
+            phoneNumber: "+16508250515",
+        }*/
